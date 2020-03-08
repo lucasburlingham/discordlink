b1ce4398ada1fd5f949099d0f54acb5a8bc7b280
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2020-03-08 15:33:15 -0400

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
-local color = minetest.settings:get("color")
-
-if color == nil then
-    color = "red"
-end
-
+-- mod by simondanerd, diegojsrw, NoNameDude
 
 minetest.register_chatcommand("discordlink", {
     privs = {shout=true},
@@ -12,7 +7,7 @@ minetest.register_chatcommand("discordlink", {
     if not player then
         return 
     end
-    minetest.chat_send_player(name, minetest.colorize(color, "Discord Permalink: https://discord.gg/SGwQ4v9"))
+    minetest.chat_send_player(name, "Discord Permalink: https://discord.gg/SGwQ4v9")
     end,
 })
 
@@ -23,6 +18,20 @@ minetest.register_chatcommand("discordlink_admin", {
     if not player then
         return
     end
-    minetest.chat_send_all(minetest.colorize(color,"Discord Permalink: https://discord.gg/SGwQ4v9"))
+    minetest.chat_send_all("Discord Permalink: https://discord.gg/SGwQ4v9")
+    end
+})
+
+
+minetest.register_privilege("discord_link", { description = "Allows player to send others a discordlink" })
+
+minetest.register_chatcommand("discordlink_sendto", {
+    privs = {discord_link=true},
+    func = function(name, param)
+    local player = minetest.get_player_by_name(param)
+    if not player then
+        return true, "Sorry, the Player referenced is not online."
+    end
+    minetest.chat_send_player(param, "Discord Permalink: https://discord.gg/SGwQ4v9")
     end
 })
