2a009ec03c45fc65824f319b0d9e469198783631
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2020-03-08 12:35:09 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,3 +6,13 @@ minetest.register_chatcommand("discordlink", {
         return true, "Discord Permalink: https://discord.gg/SGwQ4v9"
     end,
 })
+
+minetest.register_chatcommand("discordlink_admin", {
+    privs = {
+    admin = true,
+    },
+    func = function(name, param)
+        minetest.chat_send_all("Discord Permalink: https://discord.gg/SGwQ4v9")
+        return true, "Sent for all players"
+    end,
+})
