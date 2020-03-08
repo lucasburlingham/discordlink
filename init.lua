d4d72e0e2e27426dbeb124cf938599a6ce6b09bd
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2020-03-08 14:05:19 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,28 @@
+local color = minetest.settings:get("color")
+
+if color == nil then
+    color = "red"
+end
+
+
 minetest.register_chatcommand("discordlink", {
-    privs = {
-	shout = true,
-    },
-    func = function(name, param)
-        return true, "Discord Permalink: https://discord.gg/SGwQ4v9"
+    privs = {shout=true},
+    func = function(name)
+    local player = minetest.get_player_by_name(name)
+    if not player then
+        return 
+    end
+    minetest.chat_send_player(name, minetest.colorize(color, "Discord Permalink: https://discord.gg/SGwQ4v9"))
     end,
 })
 
 minetest.register_chatcommand("discordlink_admin", {
-    privs = {
-    server = true,
-    },
-    func = function(name, param)
-        minetest.chat_send_all("Discord Permalink: https://discord.gg/SGwQ4v9")
-        return true, "Sent for all players"
-    end,
+    privs = {server=true},
+    func = function(name)
+    local player = minetest.get_player_by_name(name)
+    if not player then
+        return
+    end
+    minetest.chat_send_all(minetest.colorize(color,"Discord Permalink: https://discord.gg/SGwQ4v9"))
+    end
 })
